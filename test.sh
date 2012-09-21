5ee99d97ed9730c23f6cc4c5f79dc4ce86039232
sergey <rostov.s@bookvoed.ru>
2012-09-21 16:18:48 +0400

    test

--- test.sh
+++ test.sh
@@ -2,11 +2,15 @@
 
 if [ "$(id -u)" != "0" ]; then
 	echo "Sorry, you are not root."
-	echo "Try: curl -s https://raw.github.com/bookvoed/team/master/README.md | bash"
+	echo "Try: wget -qO - https://raw.github.com/bookvoed/team/master/new-workspace.sh | bash"
 	exit 1
 fi
 
-WEBDEV_SERVER=10.0.5.221
+# mount nfs
+mount 10.0.5.221:/srv/homes/web /mnt/web
 
 # configure apt cache
-echo 'Acquire::http::Proxy "http://10.0.5.221:3142/apt-cacher/"' > /etc/apt/apt.conf.d/00apt-cacher;
+echo 'Acquire::http::Proxy "http://10.0.5.221:3142/apt-cacher/";' > /etc/apt/apt.conf.d/00apt-cacher
+
+add-apt-repository -y ppa:ondrej/php5
+apt-get upgrade
