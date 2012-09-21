19574664de62c3cf9319e5879c2eefd13b4e644e
sergey <rostov.s@bookvoed.ru>
2012-09-21 15:05:28 +0400

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #/usr/bin/bash
 
-echo "It's works!"
+if [ "$(id -u)" != "0" ]; then
+	echo "Sorry, you are not root."
+	echo "Try: curl -s https://raw.github.com/bookvoed/team/master/README.md | bash"
+	exit 1
+fi
