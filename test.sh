47d70791b9355a3889d39e20e05cdacfa4d8cd23
sergey <rostov.s@bookvoed.ru>
2012-09-21 16:22:34 +0400

    test

--- test.sh
+++ test.sh
@@ -7,10 +7,12 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # mount nfs
+mkdir /mnt/web
 mount 10.0.5.221:/srv/homes/web /mnt/web
 
 # configure apt cache
 echo 'Acquire::http::Proxy "http://10.0.5.221:3142/apt-cacher/";' > /etc/apt/apt.conf.d/00apt-cacher
 
 add-apt-repository -y ppa:ondrej/php5
-apt-get upgrade
+apt-get -y update
+apt-get -y upgrade
