29acb7371e24997dc6c1907b438fab05db3c743a
sergey <rostov.s@bookvoed.ru>
2012-09-25 14:23:19 +0400

    test

--- test.sh
+++ test.sh
@@ -6,17 +6,12 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
-# mount nfs
-mkdir /mnt/web
-mount 10.0.5.221:/srv/homes/web /mnt/web
-
 # configure apt cache
 echo 'Acquire::http::Proxy "http://10.0.5.221:3142/apt-cacher/";' > /etc/apt/apt.conf.d/00apt-cacher
 
 add-apt-repository -y ppa:ondrej/php5
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install \
+apt-get upgrade
+apt-get install \
     php5 \
     php5-dev\
     php5-dbg\
@@ -45,30 +40,7 @@ apt-get -y install \
     rubygems\
     python-setuptools
 
-pear config-set auto_discover 1
-pear install --alldeps \
-   pear.phpunit.de/PHPUnit \
-   phpunit/DbUnit \
-   phpunit/PHPUnit_Selenium \
-   phpunit/PHPUnit_Story \
-   phpunit/PHPUnit_SkeletonGenerator \
-   phpunit/PHPUnit_TestListener_DBUS \
-   phpunit/PHPUnit_TestListener_XHProf \
-   phpunit/PHPUnit_TicketListener_Trac \
-   phpunit/PHP_Invoker \
-   VersionControl_Git \
-   Archive_Tar \
-   pear.pdepend.org/PHP_Depend-beta \
-   pear.phpmd.org/PHP_PMD \
-   phpunit/phpcpd \
-   phpunit/phpdcd \
-   pear.phpdoc.org/phpDocumentor-alpha \
-   pear.phing.info/phing \
-   Image_GraphViz \
-   pear.bovigo.org/vfsStream-beta
+# mount nfs
 
-gem install \
-    taskjuggler
-
-easy_install \
- http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
+mkdir /mnt/web
+mount 10.0.5.221:/srv/homes/web /mnt/web
