05927a466bbd9da1a15cde91ef1cd6aa0469a6c5
red_led <red-led@users.noreply.github.com>
2016-10-11 17:52:35 +0300

    clean

--- go.sh
+++ go.sh
@@ -18,27 +18,19 @@ if [ "$?" -ne 0 ] || [ ! -d "/home/$username" ]; then
   exit 1
 fi
 
-apt -y purge openjdk*
 apt -y install software-properties-common curl
+#apt -y purge openjdk*
+#java
+#add-apt-repository -y ppa:webupd8team/java
 
 #git
 add-apt-repository -y ppa:git-core/ppa
-#nodejs
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#java
-add-apt-repository -y ppa:webupd8team/java
-#gimp
-add-apt-repository -y ppa:otto-kesselgulasch/gimp
-#postgres
-add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main 9.5"
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 apt -y update
 apt -y dist-upgrade
 
 apt -y install \
     automake\
-    bar\
     chromium-browser\
     cifs-utils\
     dia\
@@ -50,16 +42,16 @@ apt -y install \
     google-perftools\
     graphviz\
     kcachegrind\
-    lyx\
     make\
     mc\
-    memcached\
     msttcorefonts\
     nfs-common\
     nginx\
+    npm\
+    nodejs-legacy\
     nodejs\
     unrar\
-    oracle-java8-installer\
+    openjdk\
     password-gorilla\
     pgadmin3\
     php \
@@ -71,23 +63,18 @@ apt -y install \
     php-intl\
     php-json\
     php-mbstring\
-    php-memcached\
     php-msgpack\
     php-pgsql\
     php-redis\
     php-xdebug\
     php-xsl\
-    pidgin\
-    postgresql-9.5\
+    postgresql\
     postgresql-contrib\
     python-setuptools\
     redis-server\
     ruby\
     sphinxsearch\
-    ssh\
-    stardict\
-    thunderbird\
-    vim
+    openssh-server
 
 fc-cache -fv
 
